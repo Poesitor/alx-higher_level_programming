f8a9e75972ebcfc62829e08d3f189cc25136e81c
Poesitor <Poesitor1@gmail.com>
2024-04-01 12:57:51 +0000

    add integer specifier

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,4 +5,4 @@ def print_matrix_integer(matrix=[[]]):
     """ Prints a matrix of integers """
     for row in matrix:
         for col in row:
-            print("{}".format(col), end=" " if col != row[-1] else "\n")
+            print("{:d}".format(col), end=" " if col != row[-1] else "\n")
