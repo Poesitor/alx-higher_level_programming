6430127aaed72b004f214f55d1dec8dddd410250
Poesitor <Poesitor1@gmail.com>
2024-04-01 13:01:17 +0000

    change format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,4 +5,5 @@ def print_matrix_integer(matrix=[[]]):
     """ Prints a matrix of integers """
     for row in matrix:
         for col in row:
-            print("{:d}".format(col), end=" " if col != row[-1] else "\n")
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
