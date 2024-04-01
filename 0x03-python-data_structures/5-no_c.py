a68096fc7d3c40d2ba40098f619d8d131cfdab1c
Poesitor <Poesitor1@gmail.com>
2024-04-01 06:27:04 +0000

    Remove certain characters from string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+    if my_string:
+        result = ""
+        for i in my_string:
+            if i != 'c' and i != 'C':
+                result += i
+        return result
