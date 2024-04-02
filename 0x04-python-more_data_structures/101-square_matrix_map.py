cfe09e7e0a8860fdb66f669fd3d3c8b9aa430c50
Poesitor <Poesitor1@gmail.com>
2024-04-02 13:54:53 +0000

    keep line limit to 3

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-
-
 def square_matrix_map(matrix=[]):
-    """
-    Computes and returns a new matrix with each value equal to
-    the square of the value of the input
-    """
     return list(map(lambda sub: list(map(lambda x: x ** 2, sub)), matrix))
